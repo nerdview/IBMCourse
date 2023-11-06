84cf27862f5af723a75e5423157458dd7aa57094
Lucas Martins <146261744+nerdview@users.noreply.github.com>
2023-11-06 15:56:32 -0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DataScienceEcosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# *Objectives:*
+# * Types of ML are Supervised, Unsupervised, and Reinforcement.
+# * Supervised learning comprises two types of models, regression and classification.
+# * Deep learning refers to a general set of models and techniques that loosely emulate the way the human brain solves a wide range of problems.
+# * Community Data License Agreement makes it easier to share open data.
+# * Curiosity is the key for Data Science 
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. SQL 
+# 3. R 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# * Numpy
+# * Pandas
+# * ApacheSpark
+# * Scala
+# * Scikit-Learn
+# * Matplotlib
+
+# | Data Science Tools | 
+# | -------------| 
+# | GitHub | 
+# | DAX |
+# | Jupyter Notebook | 
+# | Hadoop | 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# * 1 + 1 = 2 
+# * 2 - 1 = 1 
+# * 3 * 3 = 9 
+# * 10 // 2 = 5
+
+# In[2]:
+
+
+# cell to multiple 
+x = (3*4)+5
+print(x)
+
+# This a simple arithmetic expression to mutiply then add integers
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+min = 200
+h = min/60
+print(h)
+
+
+# ## Author: 
+# Lucas Emanuel Souza Farias Martins
